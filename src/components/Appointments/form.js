34c2e5e6d28a15d906f941df57309ed91584282d
Hamza Warsame <hamza360123@gmail.com>
2021-12-16 20:02:29 -0500

    implement unit tests on form

--- src/components/Appointments/form.js
+++ src/components/Appointments/form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
      setStudent("")
@@ -17,6 +18,14 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
 
    return(
     <main className="appointment__card appointment__card--create">
@@ -29,8 +38,10 @@ export default function Form(props) {
         placeholder="Enter Student Name"
         value={student}
         onChange={(event) => setStudent(event.target.value)}
+        data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList 
     onChange={setInterviewer} 
     interviewers={props.interviewers}
@@ -40,7 +51,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel} >Cancel</Button>
-      <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+      <Button confirm onClick={() => validate()}>Save</Button>
     </section>
   </section>
 </main>
